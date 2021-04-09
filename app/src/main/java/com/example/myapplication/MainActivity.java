cae2bb928f2aa722d065282167bd48c1692eb73a
Froyder <homelik@mail.ru>
2021-04-09 13:55:10 +0300

    Сдаемся!

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -23,7 +23,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //setContentView(R.layout.text_layout);
 
         layout = findViewById(R.id.layout);
         Button button = findViewById(R.id.button2);
